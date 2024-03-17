9a8cd744a1bb7f2d058aae4b9e5d3499564b620e
FlowsenFresh <101529351+FlowsenFresh@users.noreply.github.com>
2024-03-17 22:14:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const gtCount = document.getElementById("gtCount");
+const ktCount = document.getElementById("ktCount");
+const expoCount = document.getElementById("expoCount");
+const startBtn = document.getElementById("startBtn");
+const gtsText = document.getElementById("gtsText");
+const ktsText = document.getElementById("ktsText");
+
+function expoCalc() {
+  gtsText.textContent = Math.trunc(gtCount.value / expoCount.value);
+  ktsText.textContent = Math.trunc(ktCount.value / expoCount.value);
+}
+
+startBtn.addEventListener("click", expoCalc);
