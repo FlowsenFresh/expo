143c08744eeb52ccb61c8462b4827a02be5f9f7c
FlowsenFresh <101529351+FlowsenFresh@users.noreply.github.com>
2024-03-17 23:13:06 +0100

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const expoCount = document.getElementById("expoCount");
 const startBtn = document.getElementById("startBtn");
 const gtsText = document.getElementById("gtsText");
 const ktsText = document.getElementById("ktsText");
+const textOutput = document.getElementById("textOutput");
 
 function expoCalc() {
   gtsText.textContent = Math.trunc(gtCount.value / expoCount.value);
